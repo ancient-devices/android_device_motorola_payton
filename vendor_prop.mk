8b2d35cc0b6018c00059b615b479c762ee062ec5
ExactExampl <64069095+ExactExampl@users.noreply.github.com>
2021-09-24 17:17:41 +0300

    payton: enable sf blurs support

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -30,7 +30,10 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # Display
 PRODUCT_PROPERTY_OVERRIDES += \
-    debug.sf.latch_unsignaled=0 
+    debug.sf.latch_unsignaled=0 \
+    ro.surface_flinger.supports_background_blur=1 \
+    persist.sys.sf.disable_blurs=1 \
+    ro.sf.blurs_are_expensive=1
     
 # FM
 PRODUCT_PROPERTY_OVERRIDES += \
